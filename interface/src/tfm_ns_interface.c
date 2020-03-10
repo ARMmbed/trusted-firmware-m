30d76e58023bd7c31700f925758adede25276668
Jaeden Amero <jaeden.amero@arm.com>
2020-03-10 12:04:08 +0000

    Core: Remove dependency on os_wrapper
    Change-Id: If459aa0f17c8f2ef2fee4851cfad17a45778ae23
    Signed-off-by: Jaeden Amero <jaeden.amero@arm.com>
    Signed-off-by: Vikas Katariya <vikas.katariya@arm.com>

--- interface/src/tfm_ns_interface.c
+++ interface/src/tfm_ns_interface.c
@@ -7,15 +7,14 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#include "os_wrapper/mutex.h"
-
 #include "tfm_api.h"
 #include "tfm_ns_interface.h"
+#include "cmsis_os2.h"
 
 /**
  * \brief the ns_lock ID
  */
-static void *ns_lock_handle = NULL;
+static osMutexId_t ns_lock_handle = NULL;
 
 __attribute__((weak))
 int32_t tfm_ns_interface_dispatch(veneer_fn fn,
@@ -23,14 +22,15 @@ int32_t tfm_ns_interface_dispatch(veneer_fn fn,
                                   uint32_t arg2, uint32_t arg3)
 {
     int32_t result;
+    osStatus_t status;
 
     /* TFM request protected by NS lock */
-    while (os_wrapper_mutex_acquire(ns_lock_handle, OS_WRAPPER_WAIT_FOREVER)
-            != OS_WRAPPER_SUCCESS);
+    while (osMutexAcquire(ns_lock_handle, osWaitForever)
+            != osOK);
 
     result = fn(arg0, arg1, arg2, arg3);
 
-    while (os_wrapper_mutex_release(ns_lock_handle) != OS_WRAPPER_SUCCESS);
+    while (osMutexRelease(ns_lock_handle) != osOK);
 
     return result;
 }
@@ -38,13 +38,22 @@ int32_t tfm_ns_interface_dispatch(veneer_fn fn,
 __attribute__((weak))
 enum tfm_status_e tfm_ns_interface_init(void)
 {
-    void *handle;
+    const osMutexAttr_t attr = {
+        .name = NULL,
+        .attr_bits = osMutexPrioInherit, /* Priority inheritance is recommended
+                                          * to enable if it is supported.
+                                          * For recursive mutex and the ability
+                                          * of auto release when owner being
+                                          * terminated is not required.
+                                          */
+        .cb_mem = NULL,
+        .cb_size = 0U
+    };
 
-    handle = os_wrapper_mutex_create();
-    if (!handle) {
+    ns_lock_handle = osMutexNew(&attr);
+    if (!ns_lock_handle) {
         return TFM_ERROR_GENERIC;
     }
 
-    ns_lock_handle = handle;
     return TFM_SUCCESS;
 }
