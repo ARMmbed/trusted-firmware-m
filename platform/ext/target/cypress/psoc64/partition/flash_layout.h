2a9cf4ed1fd230417480416a5c7d2966b3011ffd
Vikas Katariya <vikas.katariya@arm.com>
2020-04-03 14:39:04 +0100

    Add SPDX machine readable licensing identifiers
    Signed-off-by: Vikas Katariya <vikas.katariya@arm.com>

--- platform/ext/target/cypress/psoc64/partition/flash_layout.h
+++ platform/ext/target/cypress/psoc64/partition/flash_layout.h
@@ -1,6 +1,8 @@
 /*
  * Copyright (c) 2017-2019 Arm Limited. All rights reserved.
  * Copyright (c) 2019, Cypress Semiconductor Corporation. All rights reserved.
+ * 
+ * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
